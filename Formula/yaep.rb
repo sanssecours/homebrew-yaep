ac203384f2e52306452bf8c6b4f4b1d073336ab3
René Schwaiger <sanssecours@me.com>
2018-12-04 12:46:01 +0100

    Formula: Add bottle for macOS 10.14

--- Formula/yaep.rb
+++ Formula/yaep.rb
@@ -4,6 +4,13 @@ class Yaep < Formula
   url "https://github.com/sanssecours/yaep/archive/1.0.tar.gz"
   sha256 "c383b33ca5ab636b44cd2c6e231fb2ce11809a6e5408602575df18999c8b9af8"
 
+  bottle do
+    root_url("https://github.com/sanssecours/homebrew-yaep/releases/" \
+             "download/1.0")
+    sha256 "06537e17dc17b607235a2f88f4b60b12139097ede034010ca96361cdf7742c9c" \
+           => :mojave
+  end
+
   depends_on "cmake" => :build
 
   needs :cxx11
