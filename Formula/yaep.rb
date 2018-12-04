a6771b22b7694b8d6be7c7d8790f5ee07c37e8fc
René Schwaiger <sanssecours@me.com>
2018-12-04 11:41:27 +0100

    Add initial version of formula

---
+++ Formula/yaep.rb
@@ -0,0 +1,27 @@
+class Yaep < Formula
+  desc "Yet Another Earley Parser"
+  homepage "https://github.com/vnmakarov/yaep"
+  url "https://github.com/sanssecours/yaep/archive/1.0.tar.gz"
+  sha256 "c383b33ca5ab636b44cd2c6e231fb2ce11809a6e5408602575df18999c8b9af8"
+
+  depends_on "cmake" => :build
+
+  needs :cxx11
+
+  def install
+    system "cmake", ".", *std_cmake_args
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.cpp").write <<~EOS
+      #include <yaep/yaep.h>
+      int main() {
+        yaep parser;
+        return 0;
+      }
+    EOS
+    system ENV.cxx, "test.cpp", "-std=c++11", "-L#{lib}", "-lyaep++", "-o", "test"
+    system "./test"
+  end
+end
---
+++ ReadMe.md
@@ -0,0 +1,16 @@
+# YAEP Homebrew Formula
+
+This tap contains a [🍺 Homebrew](https://brew.sh) formula for the Earley parser [YAEP](https://github.com/vnmakarov/yaep). It is currently based on a fork of YAEP, since the [official repository does not list a released version of the parsing engine](https://github.com/vnmakarov/yaep/issues/22).
+
+## Installation
+
+To install YAEP just use enter the two shell commands listed below:
+
+```sh
+brew tap sanssecours/yaep
+brew install yaep
+```
+
+## Troubleshooting
+
+To check if the installation was successful please use the command `brew test yaep`.
